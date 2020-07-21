2f5fcab0950f08c89d1d2158ce922bb6e6e0b033
Matt Ericksen <ericksenm93@ymail.com>
2020-07-21 22:33:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
+  binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    10.times { string = "s" + string }
   else
     string
   end
